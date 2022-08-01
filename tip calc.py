54f961c2a67ada9f97832603cdc198e435a491a2
Daisy <92796854+daisyv1-1@users.noreply.github.com>
2022-08-01 17:58:49 +0530

    Add files via upload
    tip calculator

---
+++ tip calc.py
@@ -0,0 +1,12 @@
+print("Welcome to the tip calculator! ")
+bill=input("What was the total bill? $")
+tip=input("How much tip would you like to give? 10, 12 or 15? ")
+people=input("How many people to split the bill? ")
+print(bill)
+print(tip)
+print(people)
+
+bill=int(tip)/100+int(bill)
+result=int(bill)/int(people)
+result="{:.2f}".format(result)
+print(f"Each person should pay ${result}")
